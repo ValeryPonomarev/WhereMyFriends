540132f92d8d6db6ac4a19686540e50b82c031aa
lordpvi <lord.pvi@gmail.com>
2016-03-21 17:57:46 +0300

    1

--- app/src/main/java/com/example/drmiller/wheremyfriends/Activities/MapsActivity.java
+++ app/src/main/java/com/example/drmiller/wheremyfriends/Activities/MapsActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -39,6 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         mapsActivityController = new MapsActivityController(getApplicationContext());
+
     }
 
 
@@ -72,41 +74,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        Location myLocation = getLastKnownLocation();
+
+
+        Location myLocation = mapsActivityController.GetMyLocation();
         LatLng iAm = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
         mMap.addMarker(new MarkerOptions().position(iAm));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(iAm));
     }
 
-    
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
 
-    private Location getLastKnownLocation() {
-        if (mLocationManager == null)
-            mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
-        List<String> providers = mLocationManager.getProviders(true);
-        Location location = null;
-        for (String provider : providers) {
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return null;
-            }
-            Location l = mLocationManager.getLastKnownLocation(provider);
-            if(l == null)
-                continue;
-
-            if((location == null) || l.getAccuracy() < location.getAccuracy())
-            {
-                location = l;
-            }
         }
-        return location;
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 
     private void UpdatePoints()
